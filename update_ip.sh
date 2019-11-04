dc30ec28db7895760eaf81ac31e737e9fd9bf845
Ricardo JM Lee <ricardomlee@gmail.com>
2019-11-04 10:53:11 +0800

    compare ip addrs locally

--- update_ip.sh
+++ update_ip.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+pi=$(pwd)
 
+IPOLD=$(cat $pi/localip.txt)
+echo $IPOLD
 IP=$(ip a | grep inet | grep -v 127.0.0.1 | grep -v inet6 | awk '{print $2}' | cut -d "/" -f1)
 
-echo ${IP}
-
-ssh ricjm@115.156.xxx.xxx "./ddns.sh $IP"
+if [ "$IP" == "$IPOLD" ]
+then
+        echo "IP address not changed!" | tee -a sj.log
+        exit
+else
+#       rm $pi/localip.txt
+        echo ${IP} > $pi/localip.txt | tee -a sj.log
+        ssh ricjm@newifi "./ddns.sh $IP" | tee -a sj.log
+fi
