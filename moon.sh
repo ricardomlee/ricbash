b62040bb1a56a3d0b25a882871840e7b9002547e
Ric Li <ricardomlee@gmail.com>
2020-07-11 18:37:52 +0800

    Update moon.sh

--- moon.sh
+++ moon.sh
@@ -75,7 +75,7 @@ cd /var/lib/zerotier-one
 zerotier-idtool initmoon identity.public > moon.json
 
 #获得公网ip
-PUBLIC_IP=$(curl ipecho.net/plain)
+PUBLIC_IP=$(curl ipinfo.io/ip)
 check_ipaddr "$PUBLIC_IP"
 if [ "$?"x = "0"x ]; then
    echo "公网IP地址：$PUBLIC_IP"
